bc6aa0a5a832c0a9c34730ff74f7a4e38cbe28b5
Shawn A Alleyne <37667669+salleynealt@users.noreply.github.com>
2018-06-20 22:02:19 -0400

    Update 06/20/2018
    Coded page 7 and started reading page  8

--- TomeRater.py
+++ TomeRater.py
@@ -1,7 +1,7 @@
 """
     TomeRater Capstone by Shawn A. Alleyne
     Started: 06/18/2018
-    Last Updated: 06/19/2018 Stopped end of page 5/11
+    Last Updated: 06/20/2018 Stopped [Analysis Methods] 8/11
     
     CLASSES
         User:
@@ -10,6 +10,26 @@
             Non_Fiction:
             
 """
+def fun_isblank(prm_test_obj):
+  rtn_bol_isblank = False
+  typ_obj_type = type(prm_test_obj)
+ 
+  if typ_obj_type == str:
+    str_test = prm_test_obj.strip()
+    if str_test == "" :
+      rtn_bol_isblank = True
+  elif typ_obj_type == list:
+    if len(prm_test_obj) <= 0:
+      rtn_bol_isblank = True
+  elif typ_obj_type == dict:
+    if len(list(prm_test_obj)) <= 0:
+      rtn_bol_isblank = True
+  elif typ_obj_type == tuple:
+    if len(list(prm_test_obj)) <= 0:
+      rtn_bol_isblank = True      
+  return(rtn_bol_isblank)
+
+
 # START CLASS USER  
 class User(object):
     name   = ""
@@ -107,6 +127,26 @@ class Book(object):
             print("Invalid Rating. Ratings must be 0-4.")
 
 
+    def get_average_rating():
+        int_count = 0
+        int_tot_rate = 0
+        int_avg_rate = 0
+        
+        for curr_rate in self.ratings:
+            int_count += 1
+            try:
+                int_tot_rate += int(curr_rate)
+            except ValueError:
+                pass
+            except TypeError:
+                pass    
+
+        if int_count > 0 and int_tot_rate > 0:
+            int_avg_rate = int(int_tot_rate / int_count)
+            
+        return(int_avg_rate)
+
+    
     def __init__(self, title, isbn):
         self.title = title
         self.isbn  = isbn       
@@ -122,7 +162,13 @@ class Book(object):
         if self.title == other_book.title and self.isbn == other_book.isbn:
             rtn_equal = True
         return(rtn_equal)
+
+
+    def __hash__(self):
+        return hash((self.title, self.isbn))
+
 # END CLASS BOOK
+
 
 
 # START CLASS FICTION     
@@ -161,9 +207,63 @@ class Non_Fiction(Book):
 # END CLASS NON_FICTION
 
 
+# START CLASS NON_FICTION
+class TomeRater():
+    
+    def __init__(self):
+        self.users = {}  #email:obj_userid
+        self.books = {}  #obj_bookid:num_times_read
+
+        
+    def create_book(title,isbn):
+        rtn_obj_new_book = Book(title,isbn)
+        return(rtn_obj_new_book)
+
+
+    def create_novel(title,author,isbn):
+        rtn_obj_new_book = Fiction(title,author,isbn)
+        return(rtn_obj_new_book)
+
+
+    def create_non_fiction(title,subject,level,isbn):
+        rtn_obj_new_book = Non_Fiction(title,subject,level,isbn)
+        return(rtn_obj_new_book)
+    
+
+    def add_book_to_user(book,email,rating=None):
+        obj_user = self.user.get(email,"")
+        if not fun_isblank(str_user_name):
+            obj_user.read_book(book,rating)
+            book.add_rating(rating)
+            try:
+                int_num_reads = int(self.books.get(book,0))
+                if int_num_reads >= 0:
+                    int_num_reads += 1
+                    self.books[book] = int_num_reads  
+                else:
+                    print("Error bad number of books [add_book_to_user] " + email)       
+            except ValueError:
+                pass
+            except TypeError:
+                pass    
+        else:
+            str_mess = "No user with email " + email + " !"
+            print(str_mess)
+            
+    
+    def add_user(name,email,books=None):
+        obj_new_user = User(name,email)
+        if books != None:
+            for book in books:
+                add_book_to_user(book,email)
+
+    
+    
+    
+# END CLASS NON_FICTION
+
+
 # START MAIN
 Hulk = Fiction("Hulk Smash V21","Marvel",1215)
 print(Hulk)
 # print("Book " + Hulk.title + " by " + Hulk.author )
-
-    
