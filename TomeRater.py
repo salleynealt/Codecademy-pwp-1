f3eb6179b957d72f57f7dd58e25f3e69d751e1a6
Shawn A Alleyne <37667669+salleynealt@users.noreply.github.com>
2018-06-25 23:40:11 -0400

    Unit Tested and Data Tested
    This is a working version. The next area is to get creative pages 10 and 11.

--- TomeRater.py
+++ TomeRater.py
@@ -1,15 +1,33 @@
 """
     TomeRater Capstone by Shawn A. Alleyne
     Started: 06/18/2018
-    Last Updated: 06/20/2018 Stopped [Analysis Methods] 8/11
-    
+    Last Updated: 06/25/2018 Stopped [Get Creative] 10/11
+                  
     CLASSES
-        User:
-        Book:
-            Fiction:
-            Non_Fiction:
+        cls.User:
+          Methods:       get_email,change_email,read_book,get_average_rating
+          Construtors:   repr -> print, eq -> ==
+
+          
+        cls.Book:
+            Methods:       get_title,get_isbn,set_isbn,add_rating,get_average_rating
+            Construtors:   repr -> print, eq -> == , hash -> ??
             
+            cls.Fiction:
+                Methods:       get_author
+                Construtors:   repr -> print
+
+            cls.Non_Fiction:
+                Methods:       
+                Construtors:   repr -> print
+
+        cls.TomeRater:
+            Methods:       create_book,create_novel,create_non_fiction,add_book_to_user,add_user
+                           print_catalog,print_users,most_read_book,highest_rated_book,most_positive_user
+            Construtors:   
+           
 """
+def fun_isblank(prm_test_obj):
 def fun_isblank(prm_test_obj):
   rtn_bol_isblank = False
   typ_obj_type = type(prm_test_obj)
@@ -34,11 +52,12 @@ def fun_isblank(prm_test_obj):
 class User(object):
     name   = ""
     email  = ""
-    books  = {}
+    books  = {} #book:rating
         
     def __init__(self, name, email):
         self.name   = name
-        self.email  = email        
+        self.email  = email
+        self.books  = {}
 
 
     def get_email(self):
@@ -51,11 +70,11 @@ class User(object):
         print( str_mess)
 
 
-    def read_book(book,rating=None):
+    def read_book(self,book,rating=None):
         self.books[book] = rating
 
         
-    def get_average_rating():
+    def get_average_rating(self):
         int_tot_rating = 0
         int_avg_rating = 0
         int_curr_rating = 0
@@ -71,8 +90,8 @@ class User(object):
             except TypeError:
                 pass
             
-        if int_num_books > 0 and int_curr_rating > 0:
-            int_avg_rating = max(int(int_curr_rating / int_num_books),0)
+        if int_num_books > 0 and int_tot_rating > 0:
+            int_avg_rating = max(int(int_tot_rating / int_num_books),0)
 
         return(int_avg_rating)
 
@@ -81,11 +100,11 @@ class User(object):
         int_books = len(self.books)
         str_review = ""
         if int_books <= 0:
-            str_review = "has not reviewed any books."
+            str_review = " has not reviewed any books."
         elif int_books == 1:
-            str_review = "has reviewed one book."
+            str_review = " has reviewed one book."
         else:     
-            str_review = "has reviewed " + str(int_books) + " books."
+            str_review = " has reviewed " + str(int_books) + " books."
             
         str_mess  = "User " + self.name + " with email address " + self.email + str_review 
         return(str_mess)
@@ -93,9 +112,9 @@ class User(object):
 
     def __eq__(self, other_user):
         rtn_equal = False
-        if self.name == other_user.name and self.email == other_ser.email:
+        if self.name == other_user.name and self.email == other_user.email:
             rtn_equal = True
-        return (trn_equal)
+        return (rtn_equal)
 # END CLASS USER       
 
         
@@ -103,31 +122,31 @@ class User(object):
 class Book(object):
     title = ""
     isbn  = 0
-    ratings = []
+    ratings = [] #list of numbers 0 to 4
 
-    def get_title():
+    def get_title(self):
         return(self.title)
 
 
-    def get_isbn():
+    def get_isbn(self):
         return(self.isbn)
 
 
-    def set_isbn(isbn=0):
+    def set_isbn(self,isbn=0):
         str_mess = ""
         self.isbn = isbn
         str_mess = "The book " + self.title + " isbn was updated."
         print(str_mess)
 
 
-    def add_rating(rating=0):
+    def add_rating(self,rating=0):
         if rating >= 0 and rating <=4:
             self.ratings.append(rating)
         else:
             print("Invalid Rating. Ratings must be 0-4.")
 
 
-    def get_average_rating():
+    def get_average_rating(self):
         int_count = 0
         int_tot_rate = 0
         int_avg_rate = 0
@@ -150,10 +169,11 @@ class Book(object):
     def __init__(self, title, isbn):
         self.title = title
         self.isbn  = isbn       
+        self.ratings = []
 
 
     def __repr__(self):
-        str_mess = self.title + " - " + str(self.isbn)
+        str_mess = self.title + " has ISBN: " + str(self.isbn)
         return(str_mess)
         
 
@@ -180,7 +200,7 @@ class Fiction(Book) :
         self.author = author
 
 
-    def get_author():
+    def get_author(self):
         return(self.author)
 
 
@@ -207,32 +227,32 @@ class Non_Fiction(Book):
 # END CLASS NON_FICTION
 
 
-# START CLASS NON_FICTION
+# START CLASS TOMERATER
 class TomeRater():
     
     def __init__(self):
-        self.users = {}  #email:obj_userid
-        self.books = {}  #obj_bookid:num_times_read
+        self.users = {}  #email:obj_user
+        self.books = {}  #obj_book:num_times_read
 
         
-    def create_book(title,isbn):
+    def create_book(self,title,isbn):
         rtn_obj_new_book = Book(title,isbn)
         return(rtn_obj_new_book)
 
 
-    def create_novel(title,author,isbn):
+    def create_novel(self,title,author,isbn):
         rtn_obj_new_book = Fiction(title,author,isbn)
         return(rtn_obj_new_book)
 
 
-    def create_non_fiction(title,subject,level,isbn):
+    def create_non_fiction(self,title,subject,level,isbn):
         rtn_obj_new_book = Non_Fiction(title,subject,level,isbn)
         return(rtn_obj_new_book)
     
 
-    def add_book_to_user(book,email,rating=None):
-        obj_user = self.user.get(email,"")
-        if not fun_isblank(str_user_name):
+    def add_book_to_user(self,book,user_email,rating=None):
+        obj_user = self.users.get(user_email,"")
+        if obj_user:
             obj_user.read_book(book,rating)
             book.add_rating(rating)
             try:
@@ -251,19 +271,54 @@ class TomeRater():
             print(str_mess)
             
     
-    def add_user(name,email,books=None):
-        obj_new_user = User(name,email)
-        if books != None:
-            for book in books:
-                add_book_to_user(book,email)
-
-    
-    
-    
-# END CLASS NON_FICTION
+    def add_user(self,user_name,user_email,user_books=None):
+        obj_new_user = User(user_name,user_email)
+        self.users[user_email] = obj_new_user
+        if user_books != None:
+            for book in user_books:
+                self.add_book_to_user(book,user_email,0)
 
 
-# START MAIN
-Hulk = Fiction("Hulk Smash V21","Marvel",1215)
-print(Hulk)
-# print("Book " + Hulk.title + " by " + Hulk.author )
+    def print_catalog(self):
+        for book in self.books:
+            print(book)
+
+
+    def print_users(self):
+        for user in self.users.values():
+            print(user)
+
+                
+    def get_most_read_book(self):
+        int_highread = 0
+        obj_most_book = None
+        
+        for book, num_read in self.books.items():
+            if num_read > int_highread:
+              obj_most_book  = book
+              int_highread = num_read 
+        return(obj_most_book)
+
+
+    def highest_rated_book(self):
+        int_highrate = 0        
+        for book in self.books:
+           curr_avg = book.get_average_rating()
+           if curr_avg  > int_highrate:
+              int_highrate = curr_avg
+              obj_high_book = book
+        return(obj_high_book)
+
+
+    def most_positive_user(self):
+        int_positive = 0
+        obj_posv_usr = None
+        for curr_user in self.users.values():
+            curr_avg = curr_user.get_average_rating()
+            if curr_avg  > int_positive :
+              int_positive  = curr_avg
+              obj_posv_usr = curr_user
+        return(obj_posv_usr)
+    
+# END CLASS TOMERATER
+
